f7b1dee26ea6cb5f982f1509d8d3a7afc8b377ee
abhishek <aksharma.27mjm@gmail.com>
2023-06-05 16:14:19 +0530

    day1 -> setMatrix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,60 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int[][] matrix = {
+                {1,2,3,0}, {1,2,3,4}, {1,2,3,3}, {1,3,4,0}
+        };
+        setMatrix(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
     }
+
+    static void setMatrix (int[][] matrix) {
+        int col0 = 1;
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    if (j != 0) {
+                        matrix[0][j] = 0;
+                    }
+                    else col0 = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++){
+                if (matrix[i][j] != 0) {
+                    if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0) {
+            for (int i = 0; i < m; i++){
+                matrix[0][i] = 0;
+            }
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+
 }
